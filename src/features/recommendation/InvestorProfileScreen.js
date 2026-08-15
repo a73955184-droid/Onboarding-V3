3fa667d53e0f0acf02a1ce32325f85cf0bb271cd
DV <coderatgit@gmail.com>
2026-08-14 23:22:13 -0400

    Change navigation path on click event

--- src/features/recommendation/InvestorProfileScreen.js
+++ src/features/recommendation/InvestorProfileScreen.js
@@ -639,7 +639,7 @@ export function renderInvestorProfile(
       'click',
       () => {
         navigate(
-          'recommendation/profile-jobs'
+          'recommendation/system-fit'
         );
       }
     );
